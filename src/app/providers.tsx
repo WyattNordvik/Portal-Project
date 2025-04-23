95c5c64b8b97b091f5a1932b6d714d01a28ab593
Wyatt Nordvik <officialwyattnordvik@gmail.com>
2025-04-23 15:39:34 -0700

    commit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,9 +9,11 @@ export function Providers({ children }: { children: ReactNode }) {
     <SessionProvider>
       <div className="min-h-screen flex flex-col">
         <header className="w-full bg-white border-b shadow-sm">
-          <div className="max-w-7xl mx-auto flex justify-end items-center p-4">
-            {/* This should render your bell or TestIcon */}
-            <TestIcon />
+          <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+            {/* DEBUG BANNER */}
+            <span className="text-red-600 font-bold mr-4">🚩 HEADER RENDERED 🚩</span>
+			{/* My Icon */}
+			<TestIcon />
           </div>
         </header>
         <main className="flex-grow">{children}</main>
