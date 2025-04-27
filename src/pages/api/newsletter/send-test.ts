0f9723e6041e84562e802dd709a18a91ac6d3e6e
Wyatt Nordvik <officialwyattnordvik@gmail.com>
2025-04-27 01:20:10 -0700

    04/27 1:20am

--- src/pages/api/newsletter/send-test.ts
+++ src/pages/api/newsletter/send-test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { transporter } from "@/lib/mailer";
-import { mjml } from "mjml";
+import { mjml2html } from "mjml/lib/index.cjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
